c033d0103148629373c0e4593279aa5d3b40a7f8
Oscar Dionisio Nunez Siri <oscardns96@gmail.com>
2018-08-18 13:21:08 -0400

    Agregando configuraciones base (ocultar/mostrar)

--- app/components/VideoReport.js
+++ app/components/VideoReport.js
@@ -1,6 +1,9 @@
 /* eslint-disable react/prefer-stateless-function,no-console,no-restricted-globals,class-methods-use-this */
 import React from 'react';
 import Typography from "@material-ui/core/es/Typography/Typography";
+import Card from "@material-ui/core/es/Card/Card";
+import CardContent from "@material-ui/core/es/CardContent/CardContent";
+import CardActions from "@material-ui/core/es/CardActions/CardActions";
 import axios from "axios/index";
 import {Doughnut, Bar} from 'react-chartjs-2';
 import Button from "@material-ui/core/es/Button/Button";
@@ -13,13 +16,16 @@ import PositiveNegativeChart from './fragments/PositiveNegativeEmotionsChart'
 class VideoReport extends React.Component {
     constructor(props) {
         super(props);
+        this.handleGraphsVisible = this.handleGraphsVisible.bind(this);
+        this.handleTextVisible = this.handleTextVisible.bind(this);
         this.state = {
             api_data: [],
             report_id: window.location.href.split('/').pop(),
-            notes: ''
+            notes: '',
+            textVisible: true,
+            graphsVisible: true
         };
     }
-
     componentWillMount() {
         let URL = null;
         if (location.hostname === 'localhost' || location.hostname === '127.0.0.1') {
@@ -43,33 +49,80 @@ class VideoReport extends React.Component {
             });
     }
 
-    render() {
+    handleGraphsVisible() {
+        this.setState((prevState) => {
+            return {
+                graphsVisible: !prevState.graphsVisible
+            };
+        });
+    }
 
+    handleTextVisible() {
+        this.setState((prevState) => {
+            return {
+                textVisible: !prevState.textVisible
+            };
+        });
+    }
+
+    render() {
+        const cardStyle = {
+            margin: '58px 200px',
+        };
         return (
             <div className='container'>
                 <Typography variant="display1" gutterBottom>
                     Reporte ID: {this.state.report_id} <Button variant="outlined" color="primary" onClick={() => {
                     window.print();
-                }}> Descargar</Button>
+                }}> Descargar</Button> /
+                    <Button variant="outlined" color="primary" onClick={this.handleGraphsVisible}>
+                        {this.state.graphsVisible ? " Ocultar gráficas" : " Mostrar graficas"}</Button> /
+                    <Button variant="outlined" color="primary" onClick={this.handleTextVisible}>
+                        {this.state.textVisible ? " Ocultar summary" : " Mostrar summary"}</Button>
                 </Typography>
                 <Typography variant="display1" gutterBottom>
                     Notas del reporte: {this.state.notes}
                 </Typography>
-                <Grid container spacing={32}>
-                    <Grid item xs={12}>
-                        <TimelineChart data={this.state.api_data}/>
-                    </Grid>
+                {this.state.textVisible &&
+                    <Card style={cardStyle}>
+                        <CardContent>
+                            <div>
+                                <Typography variant="display1" gutterBottom>
+                                    SUMMARY
+                                </Typography>
+                                <Typography variant="body1" gutterBottom>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia augue a auctor
+                                    mattis.
+                                    Aenean hendrerit elit sed augue tristique varius. Fusce vitae tortor non turpis
+                                    condimentum maximus
+                                    ut sit amet leo. Fusce elementum, ipsum non rutrum dapibus, felis purus malesuada
+                                    tellus,
+                                    non iaculis tellus justo in nisl. Morbi ac molestie risus. Curabitur consequat aliquam
+                                    semper.
+                                    Donec ultricies tempor arcu tempor ullamcorper.
+                                </Typography>
+                            </div>
+                        </CardContent>
+                    </Card>
+                }
+                {this.state.graphsVisible &&
+                    <Grid container spacing={32}>
+                        <Grid item xs={12}>
+                            <TimelineChart data={this.state.api_data}/>
+                        </Grid>
 
-                    <Grid item xs={6}>
-                        <EmotionsPieChart data={this.state.api_data}/>
+                        <Grid item xs={6}>
+                            <EmotionsPieChart data={this.state.api_data}/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <EmotionsBarChart data={this.state.api_data}/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <PositiveNegativeChart data={this.state.api_data}/>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                        <EmotionsBarChart data={this.state.api_data}/>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <PositiveNegativeChart data={this.state.api_data}/>
-                    </Grid>
-                </Grid>
+                }
+                <br/>
             </div>
         );
     }
