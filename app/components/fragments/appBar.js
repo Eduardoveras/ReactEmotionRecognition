b67f997354750b6f3e9cd535c7cc07bea44138f2
Oscar Dionisio Nunez Siri <oscardns96@gmail.com>
2018-09-07 13:58:09 -0400

    Agregando foto y email de usuario cuando se ha logueado.

--- app/components/fragments/appBar.js
+++ app/components/fragments/appBar.js
@@ -10,7 +10,6 @@ import IconButton from '@material-ui/core/IconButton';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faHome, faChartBar, faFileAlt} from '@fortawesome/free-solid-svg-icons'
-import firebase from 'firebase';
 
 library.add(faHome);
 library.add(faChartBar);
@@ -30,12 +29,25 @@ const styles = {
 };
 
 function ButtonAppBar(props) {
+    let infoUser = "";
+    let userPhoto = "";
 
-  let infoUser = localStorage.getItem(localStorage.key(0));
-  infoUser = infoUser.split(',');
-  infoUser = infoUser[0].split(':');
-  infoUser.slice(1, infoUser.length - 2);
-  console.log(infoUser[1]);
+    try{
+      infoUser = localStorage.getItem(localStorage.key(0));
+      infoUser = infoUser.split(',');
+      infoUser = infoUser[0].split(':');
+      infoUser = infoUser[1].split('"');
+
+      userPhoto = localStorage.getItem(localStorage.key(0));
+      userPhoto = userPhoto.split(',');
+      userPhoto = userPhoto[2].split(':');
+      userPhoto = userPhoto[2].split('"');
+      userPhoto = "https:" + userPhoto[0];
+      console.log(userPhoto);
+  }catch(e){
+    console.log(e.toString());
+  }
+
   const { classes } = props;
   return (
     <div className={classes.root}>
@@ -45,6 +57,9 @@ function ButtonAppBar(props) {
           <Typography variant="title" color="inherit" className={classes.flex}>
               S.D.E.C
           </Typography>
+            <img src={userPhoto} style={{width: 42, borderRadius: "50%", border: '1px solid white'}}/>
+            <Typography color="inherit" variant="caption">&nbsp; | &nbsp; {infoUser[1] != null && infoUser[1]}</Typography>
+            <Typography color="inherit" variant="button"> &nbsp; | &nbsp;</Typography>
             <Button color="inherit" href="/home"> <FontAwesomeIcon icon="home"/> &nbsp;Inicio</Button>
           <Button color="inherit" href="/reports"><FontAwesomeIcon icon="chart-bar"/> &nbsp;Reportes</Button>
           <Button color="inherit" href="/home"><FontAwesomeIcon icon="file-alt"/>&nbsp;Manuales</Button>
