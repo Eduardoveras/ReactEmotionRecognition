f5986856d66abd1caf7a83265e6bb9662ff1cca9
Eduardo Veras <eduardo.storm@gmail.com>
2018-10-17 08:48:44 -0400

    Dynamic chart from settings

--- app/components/fragments/timelineChart.js
+++ app/components/fragments/timelineChart.js
@@ -1,12 +1,13 @@
 /* eslint-disable react/prefer-stateless-function,no-console,no-restricted-globals,class-methods-use-this */
 import React from 'react';
 import {Line} from 'react-chartjs-2';
+import axios from "axios";
+import {BASE_URL_PATH} from "../../constants";
 
 class timelineChart extends React.Component {
     constructor(props) {
         super(props);
         this.api_data = [];
-        console.log(this.props.data);
         this.joy_data = [];
         this.sadness_data = [];
         this.anger_data = [];
@@ -15,6 +16,20 @@ class timelineChart extends React.Component {
         this.disgust_data = [];
         this.contempt_data = [];
         this.surprise_data = [];
+        this.state={
+            settings:null
+        }
+
+    }
+
+    componentWillMount(){
+        axios.get(BASE_URL_PATH+'/settings')
+            .then((response) => {
+                this.setState({ settings: response.data[0] });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     render() {
@@ -23,9 +38,13 @@ class timelineChart extends React.Component {
             return parseFloat(e.timeStamp).toFixed(2);
         });
 
-        this.joy_data = this.api_data.map(function (e) {
-            return e.emotions.joy;
-        });
+                this.joy_data = this.api_data.map(function (e) {
+                    return e.emotions.joy;
+                });
+
+
+
+
 
         this.sadness_data = this.api_data.map(function (e) {
             return e.emotions.sadness;
@@ -55,27 +74,6 @@ class timelineChart extends React.Component {
         let data_line_chart = {
             labels: this.timestamps,
             datasets: [
-                {
-                    label: 'Joy',
-                    fill: true,
-                    lineTension: 0.1,
-                    backgroundColor: 'rgba(75,192,192,0.4)',
-                    borderColor: 'rgba(75,192,192,1)',
-                    borderCapStyle: 'butt',
-                    borderDash: [],
-                    borderDashOffset: 0.0,
-                    borderJoinStyle: 'miter',
-                    pointBorderColor: 'rgba(75,192,192,1)',
-                    pointBackgroundColor: '#fff',
-                    pointBorderWidth: 1,
-                    pointHoverRadius: 5,
-                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                    pointHoverBorderColor: 'rgba(220,220,220,1)',
-                    pointHoverBorderWidth: 2,
-                    pointRadius: 1,
-                    pointHitRadius: 10,
-                    data: this.joy_data
-                },
                 {
                     label: 'Sadness',
                     fill: true,
@@ -206,7 +204,35 @@ class timelineChart extends React.Component {
                 }
 
             ]
+
         };
+
+        if(this.state.settings) {
+            if (this.state.settings.joy_enabled) {
+                data_line_chart.datasets.push({
+                    label: 'Joy',
+                    fill: true,
+                    lineTension: 0.1,
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: 'rgba(75,192,192,1)',
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: this.joy_data
+                },)
+
+            }
+        }
 
         return (this.api_data===[]? null : <Line data={data_line_chart} width={100} height={500}
                                                  options={{
